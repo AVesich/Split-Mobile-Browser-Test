26eff2027142900cd637014c50d84963d9d6f9ff
AVesich <75646434+AVesich@users.noreply.github.com>
2024-09-01 18:25:42 -0500

    Got dragging tab views working, beginning on dropping

---
+++ Arc-Mobile-Split-View/Views/WebTabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Arc-Mobile-Split-View
+//
+//  Created by Austin Vesich on 9/1/24.
+//
+
+import SwiftUI
+
+struct WebTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
