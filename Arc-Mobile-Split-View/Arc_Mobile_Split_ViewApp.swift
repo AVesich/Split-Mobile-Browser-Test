77e646316d90c572e9b830487e0ce25719f8c274
AVesich <75646434+AVesich@users.noreply.github.com>
2024-09-01 17:20:25 -0500

    Initial Commit

---
+++ Arc-Mobile-Split-View/Arc_Mobile_Split_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arc_Mobile_Split_ViewApp.swift
+//  Arc-Mobile-Split-View
+//
+//  Created by Austin Vesich on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Arc_Mobile_Split_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
